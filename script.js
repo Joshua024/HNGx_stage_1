b9d1a55603ad06f9f59798c09247b0684f488faa
Joshua024 <adumchimajoshua@gmail.com>
2023-09-11 10:24:22 +0100

    box shadow

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // JavaScript to display current UTC time and the current day of the week
 const utcTimeElement = document.getElementById("utc-time");
-const WeekDayEl = document.querySelector(".dayOfTheWeek")
+const WeekDayEl = document.querySelector(".dayOfTheWeek");
 const currentTimeMillis = new Date().getTime();
-const weekArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const weekDay = new Date().getDay()
+const weekArray = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const weekDay = new Date().getDay();
 function render() {
-    utcTimeElement.textContent = `Current UTC Time: ${currentTimeMillis}`;
-    console.log(weekDay)
-    WeekDayEl.textContent = `Current Day of the Week: ${weekArray[weekDay]}`
+  utcTimeElement.textContent = `Current UTC Time: ${currentTimeMillis}`;
+  console.log(weekDay);
+  WeekDayEl.textContent = `Current Day of the Week: ${weekArray[weekDay]}`;
 }
 
 // Call the function to display UTC time when the page loads and the current day of the week
